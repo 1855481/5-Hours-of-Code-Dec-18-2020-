c10925ba7bef4e286132928ae3654f2c66b45be5
1855481 <72891372+1855481@users.noreply.github.com>
2020-12-18 15:49:09 -0800

    main.py

---
+++ .github/workflows/main.py
@@ -0,0 +1,27 @@
+#Partial part 
+if(len(username)<=32 and len(username)>0): 
+  return username, True
+
+else: 
+  return username, False
+def AuthLogin(username)
+#must implement this part into the mini game
+def DisplayEnemy(enemyNum)
+
+def DisplayCoin
+
+    sc.update() 
+  
+    hit_enemy.setx(hit_enemy.xcor()+hit_enemy.dx) 
+    hit_coin.sety(hit_coin.ycor()+hit_coin.dy) 
+  
+    # Gathering points in game
+    if hit_enemy.ycor *= -1
+  
+    if
+        hit_coin.dy *= +1
+
+#now I would do the scoring system 
+def Display TimesHit(timesHit, AMOUNT_OF_ENEMIES): 
+  os.system('cls')
+  print("You got " + str(len(timesHit)) + " enemie(s) hit out of " + str(AMOUNT_OF_ENEMIES) 
